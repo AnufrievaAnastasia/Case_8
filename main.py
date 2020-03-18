7ea38b11ad3d9cc6900c7bff108fb773f71d6935
AnufrievaAnastasia <anufrieva_01@mail.ru>
2020-03-18 19:58:03 +0700

    2

---
+++ main.py
@@ -0,0 +1,38 @@
+import turtle
+
+
+def get_color_choice():
+
+    color = input('Допустимые цвета заливки:\n'
+      'красный\n'
+      'синий\n'
+      'зеленый\n'
+      'желтый\n'
+      'оранжевый\n'
+      'пурпурный\n'
+      'розовый\n'
+      'Пожалуйста, введите цвет: ').lower()
+
+    list_color = ['красный', 'синий', 'зеленый', 'желтый', 'оранжевый', 'пурпурный','розовый']
+    while color not in list_color:
+        print("'", color, "' ", 'не является верным значением. Пожалуйста, повторите попытку: ', sep='')
+        color = input().lower()
+    return color
+
+def get_num_hexagons():
+    try:
+        number = int(input('Пожалуйста, введите количество шестиугольников, располагаемых в ряд: '))
+        while number > 20 or number < 4:
+            print('Оно должно быть от 4 до 20. Пожалуйста, повторите попытку: ')
+            number = int(input())
+        return number
+    except ValueError:
+        print('Оно должно быть от 4 до 20. Пожалуйста, повторите попытку: ')
+        number = get_num_hexagons()
+
+color1 = get_color_choice()
+color2 = get_color_choice()
+number = get_num_hexagons()
+
+def draw_hexagon(x, y, side_len, color):
+    
