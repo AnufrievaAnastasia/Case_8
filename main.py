fa1714a2b89b435ce8265c97d6c04e536f8d3a44
aleksandrazh20 <hollandjoe20@gmail.com>
2020-03-21 12:08:02 +0700

    123

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import turtle
-
+import math
 
 def get_color_choice():
 
     color = input('Допустимые цвета заливки:\n'
-      'красный\n'
-      'синий\n'
-      'зеленый\n'
-      'желтый\n'
-      'оранжевый\n'
-      'пурпурный\n'
-      'розовый\n'
+      'red\n'
+      'blue\n'
+      'green\n'
+      'yellow\n'
+      'orange\n'
+      'magenta\n'
+      'pink\n'
       'Пожалуйста, введите цвет: ').lower()
 
-    list_color = ['красный', 'синий', 'зеленый', 'желтый', 'оранжевый', 'пурпурный','розовый']
+    list_color = ['red', 'blue', 'green', 'yellow', 'orange', 'magenta','pink']
     while color not in list_color:
         print("'", color, "' ", 'не является верным значением. Пожалуйста, повторите попытку: ', sep='')
         color = input().lower()
@@ -34,8 +34,52 @@ color1 = get_color_choice()
 color2 = get_color_choice()
 number = get_num_hexagons()
 
-def draw_hexagon(x, y, side_len, color):
+def draw_hexagon(x, y, side_len):
+    '''
 
-    d_2 = 250
-    d_2_n = d_2 // number # это d в зависимости от количества фигур
-    s = int((3**0.5) / 2 * d_2_n) # это длина стороны шестиугольника
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param side_len: side length of a hexagon
+    :param color:
+    :return: None
+    '''
+
+    turtle.up()
+    turtle.setposition(x, y)
+    for i in range(number):
+        for j in range(number):
+            turtle.down()
+            if j % 2 == 0:
+                turtle.color(color1)
+            else:
+                turtle.color(color2)
+            turtle.left(90)
+            turtle.forward(side_len)
+            turtle.right(60)
+            turtle.forward(side_len)
+            turtle.right(60)
+            turtle.forward(side_len)
+            turtle.right(60)
+            turtle.forward(side_len)
+            turtle.right(60)
+            turtle.forward(side_len)
+            turtle.right(60)
+            turtle.forward(side_len)
+            turtle.right(150)
+            turtle.up()
+            turtle.forward(side_len*math.sqrt(3))
+        turtle.right(120)
+        turtle.forward(side_len)
+        turtle.left(30)
+        turtle.forward(side_len)
+        turtle.right(90)
+        turtle.forward((side_len*math.sqrt(3))*number)
+        turtle.right(180)
+
+
+x = int(input('Введите координату х: '))
+y = int(input('Введите координату у: '))
+d_2 = 250
+d_2_n = d_2 // number
+side_len = int((3**0.5) / 2 * d_2_n)
+draw_hexagon(x,y,side_len)
